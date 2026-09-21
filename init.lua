4ce552d64b61f8644bf295095c66747bb4a32785
Timur Zinnatulin <teemychteemych@gmail.com>
2026-09-21 17:30:43 +0300

    Added transparency to colorscheme

--- init.lua
+++ init.lua
@@ -424,12 +424,10 @@ do
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   vim.pack.add { gh 'rebelot/kanagawa.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  -- require('tokyonight').setup {
-  --   styles = {
-  --     comments = { italic = false }, -- Disable italics in comments
-  --   },
-  -- }
+  require("kanagawa").setup {
+          transparent = true,
+          theme = "default",
+  }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
